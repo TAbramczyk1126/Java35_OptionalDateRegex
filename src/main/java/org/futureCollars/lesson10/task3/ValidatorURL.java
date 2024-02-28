cefb1d47cd029880dd0606e23b4578e08fb97235
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-28 19:34:53 +0100

    Lesson 10 - Task -  ValidatorURL

--- src/main/java/org/futureCollars/lesson10/task3/ValidatorURL.java
+++ src/main/java/org/futureCollars/lesson10/task3/ValidatorURL.java
@@ -5,14 +5,13 @@ import java.util.regex.Pattern;
 
 public class ValidatorURL {
 
-    public static void main(String[] args) {
+    public boolean isURL(String URL){
 
         Pattern pattern = Pattern.compile("http://www.onet.pl|https://mail.google.com|http://wiadmosci.onet.pl|http://onet.pl");
-        Matcher matcher = pattern.matcher("https://mail.google.com");
+        Matcher matcher = pattern.matcher(URL);
 
         boolean matches = matcher.matches();
 
-        if (matches) System.out.println("URL address is correct");
-        else System.out.println("Incorrect data was entered");
+        return matches;
     }
 }
